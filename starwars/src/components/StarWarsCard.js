cbcb37cba1725c5f54c03592cf44e434b75c790d
KROHNICK <business.kanekperez@gmail.com>
2019-01-11 10:02:22 -0800

    Added css customization to app

--- starwars/src/components/StarWarsCard.js
+++ starwars/src/components/StarWarsCard.js
@@ -8,7 +8,14 @@ const StarWarsCard = props => {
       {props.characters.map((starWarsCharacter, i) => {
         return (
           <li className="StarWarsCard" key={i}>
-            {starWarsCharacter.name}
+            <h2>{starWarsCharacter.name}</h2>
+            <p>Born: {starWarsCharacter.birth_year}</p>
+            <p>Eye: {starWarsCharacter.eye_color}</p>
+            <p>Gender: {starWarsCharacter.gender}</p>
+            <p>Hair: {starWarsCharacter.hair_color}</p>
+            <p>Height: {starWarsCharacter.height}</p>
+            <p>Weight: {starWarsCharacter.mass}</p>
+            <p>Skin: {starWarsCharacter.skin_color}</p>
           </li>
         );
       })}
--- starwars/src/components/StarWarsChars.js
+++ starwars/src/components/StarWarsChars.js
@@ -2,7 +2,18 @@ import React from "react";
 
 const StarWarsChars = props => {
   let chars = props.character;
-  return <li className="Chars">{chars.name}</li>;
+  return (
+    <li className="Chars">
+      <h2>{chars.name}</h2>
+      <p>Born: {chars.birth_year}</p>
+      <p>Eye: {chars.eye_color}</p>
+      <p>Gender: {chars.gender}</p>
+      <p>Hair: {chars.hair_color}</p>
+      <p>Height: {chars.height}</p>
+      <p>Weight: {chars.mass}</p>
+      <p>Skin: {chars.skin_color}</p>
+    </li>
+  );
 };
 
 export default StarWarsChars;
